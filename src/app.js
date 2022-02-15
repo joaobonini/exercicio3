315c921d00813e440849740729f33dd9eb47ce6d
joaobonini <99485644+undefined@users.noreply.github.com>
2022-02-15 14:03:38 +0000

    First commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+  objetos=[
+    {cod:"001", nome:"Marcela", artista:"Marília Mendonça" , album:"Patroas"},
+    {cod:"002", nome:"Jonas", artista:"Gusttavo Lima" , album:"O Embaixador - The Legacy"},
+    {cod:"003", nome:"Joao", artista:"Jorge & Mateus" , album:"Terra sem Cep"},
+    {cod:"004", nome:"Marcos", artista:"Zé Neto & Cristiano" , album:"Chaaama"},
+    {cod:"005", nome:"Joana", artista:"Ícaro e Gilmar" , album:"Sextou BB"},
+  ]
+}
